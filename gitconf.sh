c5db5486b5e03adc02c00b2d83b05251314c3a21
Kevin <fanchangyong@gmail.com>
2013-12-11 17:30:55 +0800

    "Add git config files"

---
+++ gitconf.sh
@@ -0,0 +1,5 @@
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.df diff
+git config --global alias.st status
+git config --global alias.ci commit
