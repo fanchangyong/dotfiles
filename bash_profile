3ddd839165894101c5d5ef8b11c37e4b82d09466
Kevin <fanchangyong@gmail.com>
2013-12-13 23:55:05 +0800

    add some aliases

--- bash_profile
+++ bash_profile
@@ -2,3 +2,5 @@ export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
 alias 'ls=ls -G'
 alias 'grep=grep --color=auto -n'
 alias erlm='erl -man'
+alias 'mk=make'
+alias 'vi=vim'
--- gitconf.sh
+++ gitconf.sh
@@ -1,6 +1,8 @@
+# aliases,for abbreviation
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.df diff
 git config --global alias.st status
-git config --global alias.ci commit
-git config --global alias.sl log pretty=oneline
+git config --global alias.cm commit
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.last 'log -1 HEAD'
