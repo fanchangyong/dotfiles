a466cb7d5c783294b538613968095d794532b3da
Kevin <kevin@kevin-virtual-machine.(none)>
2014-01-18 11:11:30 +0800

    add git aliases

--- gitconf.sh
+++ gitconf.sh
@@ -7,5 +7,7 @@ git config --global alias.st status
 git config --global alias.cm commit
 git config --global alias.unstage 'reset HEAD --'
 git config --global alias.last 'log -1 HEAD'
+git config --global alias.pl pull
+git config --global alias.ps push
 
 git config --global core.editor `which vim`
