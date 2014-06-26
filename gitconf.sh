9cec8f6e58b734b5f001abd3b742e8675ad2c43a
kevin <fanchangyong@gmail.com>
2014-06-26 11:18:47 +0800

    a beautiful git log alias

--- gitconf.sh
+++ gitconf.sh
@@ -7,6 +7,8 @@ git config --global alias.st status
 git config --global alias.cm commit
 git config --global alias.pl pull
 git config --global alias.ps push
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 git config --global core.editor `which vim`
 git config --global color.ui true
+
