8358833fc715c39c15fb3af1658f7e9718a704bd
kevin <fanchangyong@gmail.com>
2014-12-31 10:59:26 +0800

    iterm2 plist

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ ln -f -s "$PWD/psqlrc" ~/.psqlrc
 ln -f -s "$PWD/inputrc" ~/.inputrc
 ln -f -s "$PWD/slate" ~/.slate
 ln -f -s "$PWD/pentadactylrc" ~/.pentadactylrc
+ln -f -s "$PWD/iterms.plist" ~/Library/Preferences/com.googlecode.iterm2.plist
 
 # for sshrc
 
