57c1f7dfcb134ac139b452850aaf75afbef27cbe
fanchangyong <fanchangyong@gmail.com>
2016-04-25 17:28:24 +0800

    update setup tools

--- install_tools.sh
+++ install_tools.sh
@@ -3,22 +3,28 @@
 os=`uname -s`
 
 # Install HomeBrew
-if [[ os="Darwin" ]]
+if [[ $os = "Darwin" ]]
 then
+	echo "*** Welcome to Darwin ***"
 	if [[ -z `which brew` ]]
 	then
 		echo "### Installing HomeBrew..."
 		`ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"`
 	fi
+
+	cmds=(ack tree ctags sl fortune cowsay colordiff tree)
+	for cmd in "${cmds[@]}"
+	do
+		if [[ -z `which $cmd` ]]
+		then
+			echo "### Installing $cmd"
+			brew install $cmd
+		fi
+	done
 fi
 
-cmds=(ack tree ctags sl fortune cowsay colordiff tree)
-
-for cmd in "${cmds[@]}"
-do
-	if [[ -z `which $cmd` ]]
-	then
-		echo "### Installing $cmd"
-		brew install $cmd
-	fi
-done
+if [[ $os = "Linux" ]]
+then
+	cmds="grc silversearcher-ag git"
+	sudo apt-get install $cmds
+fi
